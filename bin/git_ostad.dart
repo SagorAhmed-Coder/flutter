2e3395236de50b432ba98853fff8f6bcfe2f3d2e
sagor ahmed <sagorahmedyt07@gmail.com>
2025-08-15 20:05:09 +0600

    project init

---
+++ bin/git_ostad.dart
@@ -0,0 +1,5 @@
+import 'package:git_ostad/git_ostad.dart' as git_ostad;
+
+void main(List<String> arguments) {
+  print('Hello world: ${git_ostad.calculate()}!');
+}
---
+++ test/git_ostad_test.dart
@@ -0,0 +1,8 @@
+import 'package:git_ostad/git_ostad.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
